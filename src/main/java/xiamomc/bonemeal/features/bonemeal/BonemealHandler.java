785f4ec41ef27b30627ab8f246141b4243150be1
MATRIX-feather <midnightcarnival@outlook.com>
2024-01-07 23:55:03 +0800

    让发射器也能使用我们自定义的骨粉功能

---
+++ src/main/java/xiamomc/bonemeal/features/bonemeal/BonemealHandler.java
@@ -0,0 +1,175 @@
+package xiamomc.bonemeal.features.bonemeal;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_20_R3.block.CraftBlock;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
+import xiamomc.bonemeal.MaterialTypes;
+import xiamomc.bonemeal.XiaMoExperience;
+
+public class BonemealHandler
+{
+    /**
+     * @return 执行是否成功
+     */
+    public boolean onBonemeal(ItemStack stack, Block targetBlock)
+    {
+        if (stack.getType() != Material.BONE_MEAL) return false;
+
+        var blockMaterial = targetBlock.getType();
+
+        boolean executeSuccess = false;
+
+        if (MaterialTypes.isSmallFlower(blockMaterial))
+        {
+            executeSuccess = useOnFlower(targetBlock);
+        }
+        else if (blockMaterial == Material.SUGAR_CANE)
+        {
+            executeSuccess = useOnSugarcane(targetBlock);
+        }
+        else if (MaterialTypes.isCoral(blockMaterial))
+        {
+            executeSuccess = useOnCoral(targetBlock);
+        }
+
+        return executeSuccess;
+    }
+
+    private boolean useOnFlower(Block block)
+    {
+        var world = block.getWorld();
+
+        // Spawn item for flowers
+        world.dropItemNaturally(block.getLocation(), new ItemStack(block.getType()));
+
+        return true;
+    }
+
+    private boolean useOnSugarcane(Block block)
+    {
+        var world = block.getWorld();
+
+        // Check for total height
+        var searchLoc = block.getLocation().add(0.5, 0, 0.5);
+        var caneHeight = 0;
+
+        // Check for lower blocks
+        for (int i = -1; i > -3; i--)
+        {
+            var pos = searchLoc.clone().add(0, i, 0);
+
+            var currentBlock = world.getBlockAt(pos);
+            if (currentBlock.getType() == Material.SUGAR_CANE)
+                caneHeight++;
+            else
+                break;
+        }
+
+        // Check for upper blocks
+        Block upperBlock = null;
+
+        for (int i = 0; i < 3; i++)
+        {
+            var pos = searchLoc.clone().add(0, i, 0);
+
+            var currentBlock = world.getBlockAt(pos);
+            if (currentBlock.getType() == Material.SUGAR_CANE)
+                caneHeight++;
+            else
+            {
+                upperBlock = currentBlock;
+                break;
+            }
+        }
+
+        if (caneHeight >= 3) return false;
+
+        // Set block
+        upperBlock = upperBlock == null ? world.getBlockAt(searchLoc) : upperBlock;
+
+        if (upperBlock.getType().isAir())
+        {
+            upperBlock.setType(Material.SUGAR_CANE);
+            return true;
+        }
+
+        return false;
+    }
+
+    private final CustomCoralFeature customCoralFeature = new CustomCoralFeature(NoneFeatureConfiguration.CODEC);
+
+    private boolean useOnCoral(Block block)
+    {
+        var world = block.getWorld();
+        var serverLevel = ((CraftBlock)block).getHandle().getMinecraftWorld();
+
+        var bukkitPos = block.getLocation();
+        var blockLoc = new BlockPos(bukkitPos.getBlockX(), bukkitPos.getBlockY(), bukkitPos.getBlockZ());
+
+        // Check blocks
+        var blockBelow = world.getBlockAt(bukkitPos.clone().add(0, -1, 0));
+        if (!MaterialTypes.isCoralPlantable(blockBelow.getType())) return false;
+
+        // Get LevelStem
+        var logger = XiaMoExperience.getInstance().getSLF4JLogger();
+        Registry<LevelStem> dimensionRegistry = null;
+
+        try
+        {
+            dimensionRegistry = serverLevel.getServer().registryAccess()
+                    .registryOrThrow(Registries.LEVEL_STEM);
+        }
+        catch (Throwable t)
+        {
+            logger.error("Unable to place feature: %s".formatted(t.getLocalizedMessage()));
+            t.printStackTrace();
+        }
+
+        if (dimensionRegistry == null) return false;
+
+        var key = serverLevel.dimension().location();
+        LevelStem levelStem = dimensionRegistry.get(key);
+
+        if (levelStem == null)
+        {
+            logger.warn("Unable to place feature since we can't place feature at world '%s'"
+                    .formatted(serverLevel.dimension().location().toString()));
+
+            return false;
+        }
+
+        // 使用自定义的CoralFeature来允许我们指定要放置的类型
+        var material = block.getType();
+        customCoralFeature.setCoralType(this.coralToBlockMaterial(material));
+
+        ChunkGenerator generator = levelStem.generator();
+        var configuration = NoneFeatureConfiguration.INSTANCE;
+
+        // Place feature
+        return customCoralFeature.place(configuration, serverLevel,
+                generator, RandomSource.create(), blockLoc);
+    }
+
+    @Nullable
+    private Material coralToBlockMaterial(Material input)
+    {
+        return switch (input)
+        {
+            case TUBE_CORAL -> Material.TUBE_CORAL_BLOCK;
+            case BRAIN_CORAL -> Material.BRAIN_CORAL_BLOCK;
+            case BUBBLE_CORAL -> Material.BUBBLE_CORAL_BLOCK;
+            case FIRE_CORAL -> Material.FIRE_CORAL_BLOCK;
+            case HORN_CORAL -> Material.HORN_CORAL_BLOCK;
+            default -> null;
+        };
+    }
+}
--- src/main/java/xiamomc/bonemeal/features/bonemeal/BonemealListener.java
+++ src/main/java/xiamomc/bonemeal/features/bonemeal/BonemealListener.java
@@ -1,5 +1,6 @@
 package xiamomc.bonemeal.features.bonemeal;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
@@ -10,6 +11,8 @@ import net.minecraft.world.level.dimension.LevelStem;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 import org.bukkit.*;
 import org.bukkit.block.Block;
+import org.bukkit.block.Dispenser;
+import org.bukkit.block.data.Directional;
 import org.bukkit.craftbukkit.v1_20_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
@@ -18,11 +21,69 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
 import xiamomc.bonemeal.MaterialTypes;
 import xiamomc.bonemeal.XiaMoExperience;
 
+import java.util.List;
+
 public class BonemealListener implements Listener
 {
+    private final BonemealHandler handler = new BonemealHandler();
+
+    private final Logger logger = XiaMoExperience.getInstance().getSLF4JLogger();
+
+    @EventHandler
+    public void onDispense(BlockDispenseEvent e)
+    {
+        var sourceBlock = e.getBlock();
+
+        if (sourceBlock.getType() != Material.DISPENSER)
+            return;
+
+        var dispenser = (Dispenser)sourceBlock.getState();
+
+        if (!(dispenser.getBlockData() instanceof Directional directional))
+        {
+            logger.warn("A dispenser has a BlockData that's not a Directional?!");
+            return;
+        }
+
+        var facing = directional.getFacing();
+        var targetLoc = dispenser.getLocation();
+        targetLoc.setX(targetLoc.x() + facing.getModX());
+        targetLoc.setY(targetLoc.y() + facing.getModY());
+        targetLoc.setZ(targetLoc.z() + facing.getModZ());
+
+        var item = e.getItem();
+        var targetBlock = sourceBlock.getWorld().getBlockAt(targetLoc);
+
+        if (!handler.onBonemeal(item, targetBlock))
+            return;
+
+        var world = targetBlock.getWorld();
+        var location = targetBlock.getLocation().add(0.5, 0.5, 0.5);
+        world.spawnParticle(Particle.VILLAGER_HAPPY, location, 20, 0.25, 0.25, 0.25);
+
+        e.setItem(new ItemStack(Material.AIR, 64));
+
+        for (ItemStack i : dispenser.getInventory())
+        {
+            if (i == null) return;
+
+            if (i.getType() != Material.BONE_MEAL) continue;
+
+            i.setAmount(i.getAmount() - 1);
+
+            if (i.getAmount() <= 0)
+                i.setType(Material.AIR);
+
+            break;
+        }
+    }
+
+    private final List<Player> rightClicked = new ObjectArrayList<>();
+
     @EventHandler
     public void onUseItem(PlayerInteractEvent e)
     {
@@ -32,82 +93,28 @@ public class BonemealListener implements Listener
         // Check action
         if (e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
 
+        var player = e.getPlayer();
+
+        if (rightClicked.remove(player) && e.getHand() == EquipmentSlot.OFF_HAND)
+            return;
+
         // Get block and its material etc.
         var block = e.getClickedBlock();
         assert block != null;
 
-        var material = block.getType();
-        var world = block.getWorld();
-
-        // Check and consume item
-        var player = e.getPlayer();
-        var playerGamemode = player.getGameMode();
         var item = e.getItem();
 
         if (item == null) return;
         if (item.getType() != Material.BONE_MEAL) return;
 
-        if (MaterialTypes.isSmallFlower(material))
-        {
-            // Spawn item for flowers
-            world.dropItemNaturally(block.getLocation(), new ItemStack(material));
-        }
-        else if (material == Material.SUGAR_CANE)
-        {
-            // Check for total height
-            var searchLoc = block.getLocation().add(0.5, 0, 0.5);
-            var caneHeight = 0;
-
-            // Check for lower blocks
-            for (int i = -1; i > -3; i--)
-            {
-                var pos = searchLoc.clone().add(0, i, 0);
-
-                var currentBlock = world.getBlockAt(pos);
-                if (currentBlock.getType() == Material.SUGAR_CANE)
-                    caneHeight++;
-                else
-                    break;
-            }
-
-            // Check for upper blocks
-            Block upperBlock = null;
-
-            for (int i = 0; i < 3; i++)
-            {
-                var pos = searchLoc.clone().add(0, i, 0);
-
-                var currentBlock = world.getBlockAt(pos);
-                if (currentBlock.getType() == Material.SUGAR_CANE)
-                    caneHeight++;
-                else
-                {
-                    upperBlock = currentBlock;
-                    break;
-                }
-            }
-
-            if (caneHeight >= 3) return;
-
-            // Set block
-            upperBlock = upperBlock == null ? world.getBlockAt(searchLoc) : upperBlock;
-
-            if (upperBlock.getType().isAir())
-                upperBlock.setType(Material.SUGAR_CANE);
-            else
-                return;
-        }
-        else if (MaterialTypes.isCoral(material))
-        {
-            if (!useOnCoral(player, block)) return;
-        } else
-        {
-            // Not flower or sugar cane, return
+        if (!handler.onBonemeal(item, block))
             return;
-        }
-
-        if (playerGamemode == GameMode.SURVIVAL || playerGamemode == GameMode.ADVENTURE)
+        else
             item.setAmount(item.getAmount() - 1);
+
+        // Add to block
+        if (e.getHand() == EquipmentSlot.HAND)
+            rightClicked.add(player);
 
         // Determine which hand to swing
         var hand = e.getHand();
@@ -117,82 +124,10 @@ public class BonemealListener implements Listener
         else
             player.swingOffHand();
 
-        // Spawn particle
+        // Spawn particle and play sound
+        var world = block.getWorld();
         var location = block.getLocation().add(0.5, 0.5, 0.5);
         world.spawnParticle(Particle.VILLAGER_HAPPY, location, 20, 0.25, 0.25, 0.25);
-
-        // Play sound
         world.playSound(location, Sound.ITEM_BONE_MEAL_USE, 1, 1);
-    }
-
-    private final CustomCoralFeature customCoralFeature = new CustomCoralFeature(NoneFeatureConfiguration.CODEC);
-
-    private boolean useOnCoral(Player player, Block clickedBlock)
-    {
-        // Check player and Level instance
-        if (!(player instanceof CraftPlayer craftPlayer)) return false;
-
-        var world = craftPlayer.getHandle().level();
-
-        if (!(world instanceof ServerLevel serverLevel)) return false;
-
-        var bukkitPos = clickedBlock.getLocation();
-        var blockLoc = new BlockPos(bukkitPos.getBlockX(), bukkitPos.getBlockY(), bukkitPos.getBlockZ());
-
-        // Check blocks
-        var blockBelow = craftPlayer.getWorld().getBlockAt(bukkitPos.clone().add(0, -1, 0));
-        if (!MaterialTypes.isCoralPlantable(blockBelow.getType())) return true;
-
-        // Get LevelStem
-        var logger = XiaMoExperience.getInstance().getSLF4JLogger();
-        Registry<LevelStem> dimensions = null;
-        try
-        {
-            dimensions = serverLevel.getServer().registryAccess()
-                    .registryOrThrow(Registries.LEVEL_STEM);
-        }
-        catch (Throwable t)
-        {
-            logger.error("Unable to place feature: %s".formatted(t.getLocalizedMessage()));
-            t.printStackTrace();
-        }
-
-        if (dimensions == null) return false;
-
-        var key = serverLevel.dimension().location();
-        LevelStem levelStem = dimensions.get(key);
-
-        if (levelStem == null)
-        {
-            logger.warn("Unable to place feature since we can't place feature at world '%s'"
-                    .formatted(serverLevel.dimension().location().toString()));
-
-            return false;
-        }
-
-        // 使用自定义的CoralFeature来允许我们指定要放置的类型
-        var material = clickedBlock.getType();
-        customCoralFeature.setCoralType(this.toBlockMaterial(material));
-
-        ChunkGenerator generator = levelStem.generator();
-        var configuration = NoneFeatureConfiguration.INSTANCE;
-
-        // Place feature
-        return customCoralFeature.place(configuration, serverLevel,
-                generator, RandomSource.create(), blockLoc);
-    }
-
-    @Nullable
-    private Material toBlockMaterial(Material input)
-    {
-        return switch (input)
-        {
-            case TUBE_CORAL -> Material.TUBE_CORAL_BLOCK;
-            case BRAIN_CORAL -> Material.BRAIN_CORAL_BLOCK;
-            case BUBBLE_CORAL -> Material.BUBBLE_CORAL_BLOCK;
-            case FIRE_CORAL -> Material.FIRE_CORAL_BLOCK;
-            case HORN_CORAL -> Material.HORN_CORAL_BLOCK;
-            default -> null;
-        };
     }
 }
